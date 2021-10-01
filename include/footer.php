4ece6054a0aefd1e2d6c98068d7f0114e9c5c28a
Andrei Shantsev <Andrei>
2021-10-02 01:59:56 +0500

    html

---
+++ include/footer.php
@@ -0,0 +1,9 @@
+<footer></footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="/js/lightgallery.js"></script>
+<script src="/js/main.js"></script>
+<script src="/js/connect_css.js"></script>
+
+</body>
+</html>
---
+++ pages/contacts.php
@@ -0,0 +1,33 @@
+<? $content["title"] = "Контакты";
+require_once $_SERVER["DOCUMENT_ROOT"]. "/include/header.php";?>
+
+<section>
+    <div class="container ">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>    
+</section>
+
+<script>
+        (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn-ru.bitrix24.ru/b18736646/crm/site_button/loader_3_7o5uze.js');
+</script>
+
+<? require_once $_SERVER["DOCUMENT_ROOT"]. "/include/footer.php"?>
+
